71207525129d869cd9a4c609b68d487166a1d30e
Lucas-Nun <lucasnunney@gmail.com>
2024-11-26 15:02:13 +0000

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/page.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
